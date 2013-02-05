eb5bd2cb5a2dabf51f789a392ce179068acf0a4b
Maurice Fonk <git@naneau.net>
2013-02-05 16:29:23 +0100

    fixing autoloader for new location

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -14,7 +14,7 @@ spl_autoload_register(function ($className) {
     }
 
     // Simple transliteration
-    require_once __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
+    require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
 });
 
 // Require composer autoloader
